bce0ae26258f46c250ddbc2bfee163aa41a7f4a9
Wankyu Choi <wankyuchoi@gmail.com>
2023-08-17 13:10:20 +0900

    settings.py added

---
+++ settings.py
@@ -0,0 +1,23 @@
+# Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
+
+APIKEY = "YOUR-OPENAI-APIKEY"
+
+# not yet available: gpt-4-32k
+
+GPT_MODEL = "gpt-3.5-turbo-16k"
+MAX_TOKENS = 4096
+DATA_FOLDER = "data/"
+CUSTOM_INSTRUCTIONS = "data/cwk.md"
+
+CUSTOM_INSTRUCTIONS_ONLY = True
+
+MAX_PROMPT_LENGTH = 200
+
+KOREAN_OCR_ENABLED = False
+
+DEBUG_MODE = True
+
+TEST_FILE = CUSTOM_INSTRUCTIONS
+TEST_URL = "https://github.com/neobundy/Annals-of-Conversations-with-Pippa/blob/main/README.md"
+TEST_IMAGE = "data/test_image.png"
+TEST_PDF = "data/test_pdf.pdf"
